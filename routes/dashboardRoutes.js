d309c2d8c96ed2dd972a11b07398a4d2b8420c86
Aayush Doshi <140516073+Aayush0603@users.noreply.github.com>
2026-02-13 17:03:54 +0530

    Update dashboardRoutes.js

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { getDashboard } = require("../controllers/dashboardController");
+const { getDashboard } = require("../Controllers/dashboardController");
 
 router.get("/", getDashboard);
 
