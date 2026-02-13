1ab1b488de75d357f5a9372796f1f6c5dea5e398
Aayush Doshi <140516073+Aayush0603@users.noreply.github.com>
2026-02-13 16:20:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,11 +17,15 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-// ✅ VERY IMPORTANT
 app.use("/api/auth", authRoutes);
 app.use("/api/workers", workerRoutes);
 app.use("/api/attendance", attendanceRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-app.listen(5000, () => console.log("Server running on port 5000 🚀"));
+// ✅ FIXED FOR DEPLOYMENT
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} 🚀`);
+});
