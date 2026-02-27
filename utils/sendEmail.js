e616653c0d81faa472276e91dfcb9e53dc87a359
Aayush <aayushdoshi126@gmail.com>
2026-02-27 18:15:59 +0530

    Updated Email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,20 +1,34 @@
-const nodemailer = require("nodemailer");
+const axios = require("axios");
 
 const sendEmail = async (to, subject, text) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  try {
+    await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: {
+          email: process.env.BREVO_SENDER_EMAIL,
+          name: "FactoryFlow"
+        },
+        to: [
+          {
+            email: to
+          }
+        ],
+        subject: subject,
+        textContent: text
+      },
+      {
+        headers: {
+          "api-key": process.env.BREVO_API_KEY,
+          "Content-Type": "application/json"
+        }
+      }
+    );
 
-  await transporter.sendMail({
-    from: `"FactoryFlow" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    text,
-  });
+  } catch (error) {
+    console.error("Brevo email error:", error.response?.data || error.message);
+    throw error;
+  }
 };
 
 module.exports = sendEmail;
